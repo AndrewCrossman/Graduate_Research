34dd0d094ac3bb46c99b646e7254054bdb16169f
Andrew Crossman <crossman@udel.edu>
2024-07-28 23:15:01 -0400

    Name Updates

---
+++ Tests/Au50Pd50_Profiles/Find_Chi_Squared.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 10:01:06 2024
+@author: crossman
+"""
+#%%
+'''
+IMPORTS
+'''
+# Library Imports
+import os                       #useful for importing other scripts
+import shutil                   #allows uploading files to drive
+import glob                     #useful for collecting and organizing files
+import csv
+import numpy as np              #math library
+import pandas as pd             #data organization library
+import scipy as sp              #math library
+from scipy import interpolate
+from astropy.coordinates import cartesian_to_spherical
+import re                       #string processing library
+from scipy import optimize      #math fitting library
+import matplotlib.pylab as plt  #allows for plotting
+# Script Imports
+import Functions
+#%%
+'''
+Important Variables Go Here
+'''
+#%%
+'''
+Find RMC Run Directories
+'''
+RMC_runs_path = "C:\\Users\\crossman\\Desktop\Reverse_Monte_Carlo\\Tests\\Au50Pd50_Profiles\\Dt500\\"
+RMC_runs = glob.glob(RMC_runs_path+"*1-20-25_v*[!Results]")    # [!Results] excludes Results in the string when searching
+print(RMC_runs)
+#%%
+'''
+Extract all relevant chi squared data from log file
+'''
+fig1, ax1 = plt.subplots(figsize=(6,4,),dpi=200)
+left, bottom, width, height = [0.5, 0.5, 0.45, 0.35]
+ax2 = fig1.add_axes([left, bottom, width, height])
+for path in RMC_runs:
+    RMC_data = Functions.get_Chi_Squared(path+"\\rmc_suite.log")
+    print(len(RMC_data))
+    ax1.plot([x *25/4 for x in range(len(RMC_data))],RMC_data, color='blue', alpha=0.5)
+    ax2.plot([x *25/4 for x in range(len(RMC_data))][20:60],RMC_data[20:60], color='blue', alpha=0.5)
+# Plot settings
+ax1.set_title("{} ".format(RMC_runs_path.split('\\')[-2])+r'$\chi^2$ Evolution', fontsize=20)
+ax1.set_ylabel(r'$\chi^2$', fontsize=16)
+ax1.tick_params(axis='y', labelsize=12, direction='in', length=8)
+ax1.set_yscale('log')
+ax1.set_xlabel(r'RMC Cycles', fontsize=16)
+ax1.tick_params(axis='x', labelsize=12, direction='in', length=8)
+ax1.grid(True)
+ax1.set_axisbelow(True)
+ax2.grid(True)
+ax2.set_axisbelow(True)
+plt.tight_layout()
---
+++ Tests/Au50Pd50_Profiles/Functions.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 21 17:11:16 2024
+
+@author: crossman
+"""
+#%%
+'''
+IMPORTS
+'''
+import shutil                   #allows uploading files to drive
+import glob                     #useful for collecting and organizing files
+import csv
+from pathlib import Path        #useful for reading text from files
+import numpy as np              #math library
+import pandas as pd             #data organization library
+import scipy as sp              #math library
+from scipy import interpolate
+import re                       #string processing library
+from scipy import optimize      #math fitting library
+import matplotlib.pylab as plt  #allows for plotting
+#%%
+'''
+Reads in DISCUS stru file and formats it into a pandas dataframe. Data is then 
+processed. Percentile is used as the quantity to which to set the reduced distance to 1
+In other words, 98 would mean that a reduced distance of 1 will be at the 98th
+percentile. This helps to account for trace amounts of evaporating atoms from the 
+RMC reconstructions. Returns processed dataframe.
+'''
+def get_df(filename,percentile):
+    data = pd.read_csv(filename, skiprows=4, sep="\\s+")
+    data = data.drop(data.columns[4:], axis=1)
+    data = data.replace(',','', regex=True)
+    data = data.set_axis(["atom", "x", "y", "z"], axis=1)
+    data = data[data.atom!='VOID']
+    data["x"] = data["x"].astype(float)
+    data["y"] = data["y"].astype(float)
+    data["z"] = data["z"].astype(float)
+    data["distance"] = np.sqrt((data["x"]*data["x"] + data["y"]*data["y"] + data["z"]*data["z"])*(4.0789*4.0789))
+    data["reduced_distance"] = data["distance"]*(1/np.percentile(data["distance"], percentile)) #assumes less than 1% of atoms have evaporated off surface
+    # Output relevant structural info
+    print("Min-Max distance in angstroms: {} - {}".format(str(round(data["distance"].min(),5)),str(round(data["distance"].max(),5))))
+    return data
+
+#%%
+'''
+Reads in a processed pandas dataframe and finds the composition profile. The number
+of bins to use is numb_bins and the number of atoms per bin is atoms_per_bin.
+Returns the composition (y) at distance (x)
+'''
+def get_Comp_by_atom_and_abs_dist(dataframe, num_bins, atoms_per_bin):
+    x, y = [], []
+    for i in range(num_bins):
+        RMC_temp_red = dataframe.nsmallest(atoms_per_bin*(i+1), "reduced_distance")[-atoms_per_bin:]
+        x.append(RMC_temp_red["reduced_distance"].mean())
+        #x.append(RMC_temp_red["reduced_distance"].iloc[-1])
+        y.append(len(RMC_temp_red[RMC_temp_red["atom"]=='AU']) / len(RMC_temp_red))
+    return x,y
+#%%
+'''
+Reads in a file of text and finds every chi_squared value. Outputs an array of 
+the chi_squared values in chronological order.
+'''
+def get_Chi_Squared(filename):
+    file_text = Path(filename).read_text()
+    print(filename)
+    str_to_search = "s2x2"
+    print(file_text)
+    '''
+    NEEDS TO BE FIXED
+    '''
+    #all_X2s = re.findall(fr"(?m)^{str_to_search}\s+(-?\d+\.?\d*)", file_text)
+    #all_X2s = re.findall("s2x2:\s+(-?\d+\.?\d\d*)", file_text)
+    all_X2s = np.array(re.findall("s2x2:\ *(\d+\.?\d*[Ee]?-?\d+)", file_text),dtype=float)
+    print(all_X2s)
+    return all_X2s
+#%%
+
